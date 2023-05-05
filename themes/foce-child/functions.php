0c6212c22ee62dfaf0a2a702885abf3a6744286a
nikosvdhgit config --global user.name nikosvdhgit config --global user.name nikosvdh <nikolavdh@gmail.com>
2023-05-05 12:49:54 +0200

    fade in sections + flowers rotation

---
+++ themes/foce-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/css/style.css'); // css child
+    wp_enqueue_style('child-script', get_stylesheet_directory_uri() . '/assets/js/script.js'); // js child
+}
+
+
+// Get customizer options form parent theme
+if (get_stylesheet() !== get_template()) {
+    add_filter('pre_update_option_theme_mods_' . get_stylesheet(), function ($value, $old_value) {
+        update_option('theme_mods_' . get_template(), $value);
+        return $old_value; // prevent update to child theme mods
+    }, 10, 2);
+    add_filter('pre_option_theme_mods_' . get_stylesheet(), function ($default) {
+        return get_option('theme_mods_' . get_template(), $default);
+    });
+}

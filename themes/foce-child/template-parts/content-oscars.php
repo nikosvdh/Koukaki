d31457617528e37b8c67f8a04e5702607490785e
nikosvdhgit config --global user.name nikosvdhgit config --global user.name nikosvdh <nikolavdh@gmail.com>
2023-05-08 14:32:54 +0200

    fix oscars_img

--- themes/foce-child/template-parts/content-oscars.php
+++ themes/foce-child/template-parts/content-oscars.php
@@ -1,14 +1,10 @@
-<?php
-
-?>
-
 <section>
     <div class="oscars_div">
         <p class="oscars_text">
             Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2023 !
         </p>
         <img class="oscars_img"
-            src="wp-content/themes/foce-child/assets/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021 1.png"
+            src="wp-content/themes/foce-child/assets/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2023.png"
             alt="logo des Oscars Short Film Animated 2023" />
     </div>
 </section>
